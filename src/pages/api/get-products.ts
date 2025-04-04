37ceddd1ba00f309bfa1a9dd45babe7281930700
Álvaro Gil <alvarogil91@gmail.com>
2025-04-04 12:35:14 +0200

    Test6

--- src/pages/api/get-products.ts
+++ src/pages/api/get-products.ts
@@ -1,23 +1,24 @@
 import type { APIRoute } from "astro";
+import { get } from 'lodash'
 import app from "../../firebase/server";
 import { getFirestore } from "firebase-admin/firestore";
 
 export const POST: APIRoute = async ({ request, locals }) => {
-    const { slug } = await request.json()
-    if (!slug) {
-        return new Response(JSON.stringify({ error: "Missing ID" }), { status: 400 });
-    }
-    try {
-        return new Response(JSON.stringify(locals))
-        const db = getFirestore(app(locals?.runtime?.env || {}));
-        const productsRef = db.collection("products");
-        const productSnap = await productsRef.get()
-        const product = productSnap.docs[0]?.data()
-        return new Response(JSON.stringify(product), { status: 200 })
-    } catch (error) {
-        console.log(error)
-        return new Response("Something went wrong", {
-            status: 500,
-        });
-    }
+    return new Response(JSON.stringify({
+        locals,
+        env: get(locals, "runtime.env")
+    }))
+    // try {
+    //     return new Response(JSON.stringify(locals))
+    //     const db = getFirestore(app(locals?.runtime?.env || {}));
+    //     const productsRef = db.collection("products");
+    //     const productSnap = await productsRef.get()
+    //     const product = productSnap.docs[0]?.data()
+    //     return new Response(JSON.stringify(product), { status: 200 })
+    // } catch (error) {
+    //     console.log(error)
+    //     return new Response("Something went wrong", {
+    //         status: 500,
+    //     });
+    // }
 };
